86c23cab177ebfd54c0b43cc7f52b23d29572f43
ElJosecito <imjose0314@gmail.com>
2023-02-05 12:32:01 -0500

    Second Commit.

--- OnlineShopSol/OnlineShop.Web/Models/ContructorProductionModel.cs
+++ OnlineShopSol/OnlineShop.Web/Models/ContructorProductionModel.cs
@@ -2,5 +2,12 @@
 {
     public class ContructorProductionModel
     {
+        public DateTime CreationDate { get; set; }
+        public int CreationUser { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public int ModifyUser { get; set; }
+        public int DeleteUser { get; set; }
+        public DateTime DeleteDate { get; set; }
+        public int Deleted { get; set; }
     }
 }
