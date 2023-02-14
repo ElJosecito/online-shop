ddc7ce4d6c8d12b169ddf835a2dc4f16372844bb
ElJosecito <imjose0314@gmail.com>
2023-02-15 01:53:47 +0900

    Validacion de datos.

--- OnlineShopSol/OnlineShop.Web/Models/SuppliersModel.cs
+++ OnlineShopSol/OnlineShop.Web/Models/SuppliersModel.cs
@@ -1,12 +1,34 @@
-﻿
+﻿using System;
 
 namespace OnlineShop.Web.Models
 {
     public class SuppliersModel : ContructorProductionModel
     {
         public int SupplierId { get; set; }
-        public string CompanyName { get; set;}
-        public string ContactName { get; set;}
+
+        //
+        private string companyName { get; set; }
+        public string CompanyName 
+        {
+            get { return this.companyName; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("El campo CompanyName esta vacio");
+                }else if(value.Length > 40) 
+                {
+                    throw new ArgumentOutOfRangeException("El Nombre excedio los caracteres permitidos");
+                }
+            }
+        }
+        //
+        private string containeName { get; set; }
+        public string ContactName 
+        { 
+            get;
+            set;
+        }
         public string ContactTitle { get; set;}
         public string Address { get; set;}
         public string City { get; set;}
