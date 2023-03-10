80be49d841cab03443a7429dd82be257a39affba
ElJosecito <imjose0314@gmail.com>
2023-03-10 17:22:36 -0500

    Swagger Fix, El archivo Startup es remplazado por program en la capa de api

--- OnlineShopSol/OnlineShop.Api/Program.cs
+++ OnlineShopSol/OnlineShop.Api/Program.cs
@@ -1,8 +1,12 @@
+using OnlineShop.DAL.Interfaces;
+using OnlineShop.DAL.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<ISuppliersRepository, SuppliersRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ OnlineShopSol/OnlineShop.DAL/Repositories/SuppliersRepository.cs
@@ -0,0 +1,59 @@
+﻿
+
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using OnlineShop.DAL.Context;
+using OnlineShop.DAL.Entities;
+using OnlineShop.DAL.Exceptions;
+using OnlineShop.DAL.Interfaces;
+using OnlineShop.DAL.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineShop.DAL.Repositories
+{
+    public class SuppliersRepository : ISuppliersRepository
+    {
+        public void Add(Suppliers suppliers)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Suppliers suppliers)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Suppliers Get(int idSuppliers)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Suppliers suppliers)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        List<SuppliersModel> ISuppliersRepository.GetAll()
+        {
+            return new List<SuppliersModel>
+            {
+                new SuppliersModel()
+                {
+
+                    SupplierId = 1,
+                    CompanyName = "Uncanny",
+                    ContactName = "Jose",
+                    ContactTitle = "Lider",
+                    Address = "SomePlace In Neverland",
+                    City = "Neverland",
+                    Region = "Este",
+                    PostalCode = "22000",
+                    Country = "EveryWhere",
+                    Phone = "809-123-1234",
+                    Fax = "Uncanny@gmail.com"
+                }
+            };
+        }
+    }
+}
