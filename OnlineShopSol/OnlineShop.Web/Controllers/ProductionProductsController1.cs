8d8e20a6893940b75a459a2b9d17d199932d3441
Elian-Sencion <20220800@itla.edu.do>
2023-04-24 19:58:14 -0400

    update

--- OnlineShopSol/OnlineShop.Web/Controllers/ProductionProductsController1.cs
+++ OnlineShopSol/OnlineShop.Web/Controllers/ProductionProductsController1.cs
@@ -1,110 +1,229 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using OnlineShop.Web.Models;
+using System.Text;
 
 namespace OnlineShop.Web.Controllers
 {
-    public class ProductionProductsController1 : Controller
+    namespace OnlineShop.Web.Controllers
     {
-        // GET: ProductionProductsController1
-        public ActionResult Index()
+        public class SuppliersController : Controller
         {
-            List<ProductionProductsModel> Production = new List<ProductionProductsModel>()
-            {
+            HttpClientHandler handler = new HttpClientHandler();
+            private readonly ILogger<SuppliersController> logger;
+            private readonly IConfiguration configuration;
+            private readonly string urlBase;
 
-                new ProductionProductsModel()
+            public SuppliersController(ILogger<SuppliersController> logger, IConfiguration configuration)
+            {
+                this.logger = logger;
+                this.configuration = configuration;
+                this.urlBase = this.configuration["apiCofing:apiBase"];
+
+            }
+
+
+            // GET: SuppliersController
+            public async Task<ActionResult> Index()
+            {
+                SuppliersListResponse suppliersList = new SuppliersListResponse();
+
+                try
                 {
+                    using (var HttpClient = new HttpClient(this.handler))
+                    {
+                        var response = await HttpClient.GetAsync($"{this.urlBase}/Suppliers");
 
-                 ID = 1, Name = "MSI Zenbook 2",
-                   supplierid = 1,
-                   categoryid = 1,
-                   unitprice = 100
-                   
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string apiResult = await response.Content.ReadAsStringAsync();
 
-                },
 
-                new ProductionProductsModel()
+                            suppliersList = JsonConvert.DeserializeObject<SuppliersListResponse>(apiResult);
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+
+                    return View(suppliersList.data);
+                }
+                catch (Exception ex)
                 {
+                    this.logger.LogError("Error al obtener los suplidores.", ex.ToString());
+                }
 
-                   ID = 2, Name = "MSI Katana 500",
-                   supplierid = 2,
-                   categoryid = 2,
-                   unitprice = 200
-
-                   },
-            };
-
-            return View(Production);
-        }
-                 
-       
-        // GET: ProductionProductsController1/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: ProductionProductsController1/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ProductionProductsController1/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
+                return View();
             }
-            catch
+
+            // GET: SuppliersController/Details/5
+            public async Task<ActionResult> Details(int id)
+            {
+
+                SupplierDetailResponse detailResponse = new SupplierDetailResponse();
+
+                try
+                {
+                    using (var HttpClient = new HttpClient(this.handler))
+                    {
+                        var response = await HttpClient.GetAsync($"{this.urlBase}/Suppliers/{id}");
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string apiResult = await response.Content.ReadAsStringAsync();
+
+
+                            detailResponse = JsonConvert.DeserializeObject<SupplierDetailResponse>(apiResult);
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+
+                    return View(detailResponse.data);
+
+                }
+                catch (Exception ex)
+                {
+                    this.logger.LogError("Error al obtener el suplidor.", ex.ToString());
+                }
+
+                return View();
+            }
+
+            // GET: SuppliersController/Create
+            public ActionResult Create()
             {
                 return View();
             }
-        }
 
-        // GET: ProductionProductsController1/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ProductionProductsController1/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
+            // POST: SuppliersController/Create
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<ActionResult> Create(SupplierCreateRequest createRequest)
             {
-                return RedirectToAction(nameof(Index));
+                CommandResponse commandResponse = new CommandResponse();
+
+                try
+                {
+                    createRequest.Creation_Date = DateTime.Now;
+                    createRequest.Creation_User = 1;
+
+                    using (var HttpClient = new HttpClient(this.handler))
+                    {
+                        StringContent request = new StringContent(JsonConvert.SerializeObject(createRequest), Encoding.UTF8, "application/json");
+
+                        var response = await HttpClient.PostAsync($"{this.urlBase}/Suppliers/SaveSuppliers", request);
+
+                        string apiResult = await response.Content.ReadAsStringAsync();
+
+                        commandResponse = JsonConvert.DeserializeObject<CommandResponse>(apiResult);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+
+                            return RedirectToAction(nameof(Index));
+                        }
+                        else
+                        {
+                            ViewBag.Message = commandResponse.message;
+                            return View();
+                        }
+
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    this.logger.LogError("Error al guardar el suplidor.", ex.ToString());
+
+                    return View();
+                }
             }
-            catch
+
+            // GET: SuppliersController/Edit/5
+            public async Task<ActionResult> Edit(int id)
             {
+                SupplierDetailResponse detailResponse = new SupplierDetailResponse();
+
+                try
+                {
+                    using (var HttpClient = new HttpClient(this.handler))
+                    {
+                        var response = await HttpClient.GetAsync($"{this.urlBase}/Suppliers/{id}");
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string apiResult = await response.Content.ReadAsStringAsync();
+
+                            detailResponse = JsonConvert.DeserializeObject<SupplierDetailResponse>(apiResult);
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+
+                    return View(detailResponse.data);
+
+                }
+                catch (Exception ex)
+                {
+                    this.logger.LogError("Error al obtener el suplidor.", ex.ToString());
+                }
+
                 return View();
             }
-        }
 
-        // GET: ProductionProductsController1/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+            // POST: SuppliersController/Edit/5
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<ActionResult> Edit(SupplierUpdateRequest supplierUpdate)
+            {
+                CommandResponse commandResponse = new CommandResponse();
 
-        // POST: ProductionProductsController1/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    supplierUpdate.Modify_Date = DateTime.Now;
+                    supplierUpdate.Modify_User = 1;
+
+                    using (var HttpClient = new HttpClient(this.handler))
+                    {
+                        StringContent request = new StringContent(JsonConvert.SerializeObject(supplierUpdate), Encoding.UTF8, "application/json");
+
+                        var response = await HttpClient.PostAsync($"{this.urlBase}/Suppliers/UpdateSuppliers", request);
+
+                        string apiResult = await response.Content.ReadAsStringAsync();
+
+                        commandResponse = JsonConvert.DeserializeObject<CommandResponse>(apiResult);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+
+                            return RedirectToAction(nameof(Index));
+                        }
+                        else
+                        {
+                            ViewBag.Message = commandResponse.message;
+                            return View();
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    this.logger.LogError("Error al editar el suplidor.", ex.ToString());
+                    return View();
+                }
             }
-            catch
-            {
-                return View();
-            }
+
+
         }
     }
-}
